437aadd33107b7716aabbbac979419d52eea68c1
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2025-02-16 16:58:18 +0100

    feat: all integrators use adaptive matsubara quadrature.
    Signed-off-by: Franz R. Sattler <sattler@thphys.uni-heidelberg.de>

--- DiFfRG/include/DiFfRG/physics/integration_finiteT/integrator_finiteTq0_cpu.hh
+++ DiFfRG/include/DiFfRG/physics/integration_finiteT/integrator_finiteTq0_cpu.hh
@@ -26,7 +26,8 @@ namespace DiFfRG
 
     IntegratorFiniteTq0TBB(QuadratureProvider &quadrature_provider, const std::array<uint, 1> _grid_sizes,
                            const ctype x_extent, const ctype T, const uint max_block_size = 0)
-        : quadrature_provider(quadrature_provider), grid_sizes{{_grid_sizes[0]}}, x_extent(x_extent), m_T(T)
+        : quadrature_provider(quadrature_provider), grid_sizes{{_grid_sizes[0], 0}}, x_extent(x_extent), m_T(T),
+          manual_E(false)
     {
       ptr_x_quadrature_p = quadrature_provider.get_points<ctype>(grid_sizes[0]).data();
       ptr_x_quadrature_w = quadrature_provider.get_weights<ctype>(grid_sizes[0]).data();
@@ -36,34 +37,52 @@ namespace DiFfRG
       (void)max_block_size;
     }
 
-    void set_T(const ctype T, const ctype typical_E = 0)
+    /**
+     * @brief Set the temperature and typical energy scale of the integrator and recompute the Matsubara quadrature
+     * rule.
+     *
+     * @param T The temperature.
+     * @param E A typical energy scale, which determines the number of nodes in the quadrature rule.
+     */
+    void set_T(const ctype T, const ctype E = 0)
     {
       m_T = T;
-      m_E = is_close(typical_E, 0.) ? m_T : typical_E;
+      // the default typical energy scale will default the matsubara size to 11.
+      m_E = is_close(E, 0.) ? 10 * m_T : E;
+      manual_E = !is_close(E, 0.);
 
-      matsubara_size = quadrature_provider.get_matsubara_points<ctype>(m_T, m_E).size();
+      grid_sizes[1] = quadrature_provider.get_matsubara_points<ctype>(m_T, m_E).size();
       ptr_matsubara_quadrature_p = quadrature_provider.get_matsubara_points<ctype>(m_T, m_E).data();
       ptr_matsubara_quadrature_w = quadrature_provider.get_matsubara_weights<ctype>(m_T, m_E).data();
     }
+
+    /**
+     * @brief Set the typical energy scale of the integrator and recompute the Matsubara quadrature rule.
+     *
+     * @param E The typical energy scale.
+     */
+    void set_E(const ctype E) { set_T(m_T, E); }
 
     IntegratorFiniteTq0TBB(const IntegratorFiniteTq0TBB &other)
         : quadrature_provider(other.quadrature_provider), grid_sizes(other.grid_sizes),
           ptr_x_quadrature_p(other.ptr_x_quadrature_p), ptr_x_quadrature_w(other.ptr_x_quadrature_w),
           ptr_matsubara_quadrature_p(other.ptr_matsubara_quadrature_p),
-          ptr_matsubara_quadrature_w(other.ptr_matsubara_quadrature_w), x_extent(other.x_extent),
-          matsubara_size(other.matsubara_size), m_T(other.m_T), m_E(other.m_E)
+          ptr_matsubara_quadrature_w(other.ptr_matsubara_quadrature_w), x_extent(other.x_extent), m_T(other.m_T),
+          m_E(other.m_E), manual_E(other.manual_E)
     {
     }
 
-    template <typename... T> NT get(const ctype k, const T &...t) const
+    template <typename... T> NT get(const ctype k, const T &...t)
     {
-      const ctype S_dm1 = 2 * std::pow(M_PI, (d - 1) / 2.) / std::tgamma((d - 1) / 2.);
+      if (!manual_E) set_T(m_T, k);
+
+      constexpr ctype S_dm1 = S_d_prec<ctype>(d - 1);
       using std::sqrt, std::exp, std::log;
 
       const auto constant = KERNEL::constant(k, t...);
       return constant +
              tbb::parallel_reduce(
-                 tbb::blocked_range2d<uint, uint>(0, grid_sizes[0], 0, matsubara_size), NT(0),
+                 tbb::blocked_range2d<uint, uint>(0, grid_sizes[0], 0, grid_sizes[1]), NT(0),
                  [&](const tbb::blocked_range2d<uint, uint> &r, NT value) -> NT {
                    for (uint idx_x = r.rows().begin(); idx_x != r.rows().end(); ++idx_x) {
                      const ctype q = k * sqrt(ptr_x_quadrature_p[idx_x] * x_extent);
@@ -80,7 +99,8 @@ namespace DiFfRG
 
                        value +=
                            int_element * weight * (KERNEL::kernel(q, q0, k, t...) + KERNEL::kernel(q, -q0, k, t...));
-                       if (idx_y == 0) value += int_element * x_weight * m_T * KERNEL::kernel(q, (ctype)0, k, t...);
+                       if (m_T > 0 && idx_y == 0)
+                         value += int_element * x_weight * m_T * KERNEL::kernel(q, (ctype)0, k, t...);
                      }
                    }
                    return value;
@@ -88,7 +108,7 @@ namespace DiFfRG
                  [&](NT x, NT y) -> NT { return x + y; });
     }
 
-    template <typename... T> std::future<NT> request(const ctype k, const T &...t) const
+    template <typename... T> std::future<NT> request(const ctype k, const T &...t)
     {
       return std::async(std::launch::deferred, [=, this]() { return get(k, t...); });
     }
@@ -100,11 +120,11 @@ namespace DiFfRG
 
     const ctype x_extent;
     ctype m_T, m_E;
+    bool manual_E;
 
     const ctype *ptr_x_quadrature_p;
     const ctype *ptr_x_quadrature_w;
     const ctype *ptr_matsubara_quadrature_p;
     const ctype *ptr_matsubara_quadrature_w;
-    uint matsubara_size;
   };
 } // namespace DiFfRG
